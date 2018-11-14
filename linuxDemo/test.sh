c35e389a623fcfdc00efcdf34f14c061f5c93e3a
qixin <qixin@haoqix.com>
2018-11-14 14:19:55 +0800

    linux demo add

--- linuxDemo/test.sh
+++ linuxDemo/test.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-echo "Hello World!"
-myUrl="http://www.google.com"
-unset myUrl
-echo $myUrl
-your_name='runoob'
-str="Hello, I know you are \"$your_name\"! \\n"
-echo $str
+echo "new test system reinput" >> ./myfile
+cat ./myfile
---
+++ linuxDemo/test1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+url="http://ww.runoob.com"
+
